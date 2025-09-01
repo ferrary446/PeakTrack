9c81652e1c403838809bb03997495d4493ff7925
Ilya Yushkov <ilyayushkov4@gmail.com>
2025-09-01 17:03:00 +0200

    Initial Commit

---
+++ PeakTrack/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  PeakTrack
+//
+//  Created by Ilya Yushkov on 01.09.2025.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
