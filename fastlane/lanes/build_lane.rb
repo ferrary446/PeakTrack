dc933dfedefc7a4f48b1a9fb40e29a77bbc7aae9
Ilya Yushkov <ilyayushkov4@gmail.com>
2025-10-24 10:25:10 +0200

    Update CI files

---
+++ fastlane/lanes/build_lane.rb
@@ -0,0 +1,16 @@
+def build_lane(options)
+    setup_ci if ENV["CI"]
+
+    clear_derived_data(
+        derived_data_path: "~/Library/Developer/Xcode/DerivedData"
+    )
+
+    build_app(
+        project: "PeakTrack.xcodeproj",
+        scheme: "PeakTrack",
+        clean: true,
+        skip_package_ipa: true,
+        skip_archive: true,
+        skip_codesigning: true
+    )
+end
